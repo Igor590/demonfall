cf730f9965774c5c4dd6e5a661faf205f0dae535
igor590 <igor_590@hotmail.com>
2025-10-02 17:09:25 -0300

    Update demonfallscript.lua

--- demonfallscript.lua
+++ demonfallscript.lua
@@ -39,8 +39,7 @@ local manualNpcLocations = {
     ["Hayakawa"] = Vector3.new(919, 757, -2256), ["Vendedor de Joias (Hayakawa)"] = Vector3.new(827, 758, -2248),
     ["Sopa (Hayakawa)"] = Vector3.new(560, 755, -2382), ["Haori (Hayakawa)"] = Vector3.new(837, 757, -1988),
     ["Caverna Oni"] = Vector3.new(-737, 695, -1513), ["Okuyia Village"] = Vector3.new(-3200, 704, -1162),
-    ["Okuyia Tavern"] = Vector3.new(-3419, 705, -1578), ["Urokodaki"] = Vector3.new(-922, 846, -990),
-    ["QG Slayers"] = Vector3.new(-1834, 871, -6391)
+    ["Okuyia Tavern"] = Vector3.new(-3419, 705, -1578), ["Urokodaki"] = Vector3.new(-922, 846, -990), ["QG Slayers"] = Vector3.new(-1834, 871, -6391)
 }
 local manualNpcNames = {}
 for name, position in pairs(manualNpcLocations) do table.insert(manualNpcNames, name) end
